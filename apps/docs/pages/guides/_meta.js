afda86bec441b8ee455f61c9e9be9ae1b05e727d
Aaryan Patel <a52patel@uwaterloo.ca>
2026-02-16 00:43:16 -0500

    docs update

--- apps/docs/pages/guides/_meta.js
+++ apps/docs/pages/guides/_meta.js
@@ -1,8 +1,6 @@
 export default {
   "adding-components": "Adding Components",
   "creating-api": "Creating API Endpoints",
-  "application-review": "Application & Review System",
   database: "Database Setup",
   development: "Development Tips",
 };
-

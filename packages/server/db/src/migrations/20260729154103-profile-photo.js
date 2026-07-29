dfc30bdf7d40bd500694263e2eb88be2f0774c04
Aaryan Patel <86172867+patel-aaryan@users.noreply.github.com>
2026-07-29 11:42:01 -0400

    migration versioning

--- packages/server/db/src/migrations/20260729154103-profile-photo.js
+++ packages/server/db/src/migrations/20260729154103-profile-photo.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20260721120000-profile-photo-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20260729154103-profile-photo-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20260721120000-profile-photo-down.sql');
+  var filePath = path.join(__dirname, 'sqls', '20260729154103-profile-photo-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
